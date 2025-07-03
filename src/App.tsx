fbcfb4082b42b0eef8172cc1f078f7a4a4aaa333
kaziabulhasib <hasibkazi420@gmail.com>
2025-07-03 19:21:56 +0530

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>welcome to library management website </div>;
+  return (
+    <div>
+      <h1 className='text-red-500'>welcome to library management website</h1>{" "}
+    </div>
+  );
 }
 
 export default App;
